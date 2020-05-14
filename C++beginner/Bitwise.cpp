83ed3299603ae0c45838fcd93d3320ff14cb4844
mahinhasan56 <mahinhasan56@gmail.com>
2020-05-14 20:13:32 +0600

    Bitwise.cpp Bitwise.o

---
+++ C++beginner/Bitwise.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=32;
+int b=14;
+int c;
+
+c=a&b;
+cout<<c<<endl;
+
+c= a|b;
+cout<<c<<endl;
+
+c=a^b;
+cout<<c<<endl;
+
+c = a<<3;
+cout<<c<<endl;
+
+return 0;
+}
