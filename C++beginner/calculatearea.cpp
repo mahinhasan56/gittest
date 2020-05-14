a69024c5d3771878acd596673af92d434890163d
mahinhasan56 <mahinhasan56@gmail.com>
2020-05-14 20:35:43 +0600

    calculate area

---
+++ C++beginner/calculatearea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   double base,height;
+   cout<<"Enter base:";
+   cin>>base;
+
+   cout<<"Enter Height";
+   cin>>height;
+
+   float area=0.5 * base * height;
+   cout<<"Area of triangle = " <<area;
+   return 0;
+
+}
